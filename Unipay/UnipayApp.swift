f4b152a0dc496e737351b29f4b2aa6f12a7064fd
Bilel <bilel@powder.gg>
2022-01-15 23:50:10 +0100

    Initial Commit

---
+++ Unipay/UnipayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnipayApp.swift
+//  Unipay
+//
+//  Created by Bilel Bouricha on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnipayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
